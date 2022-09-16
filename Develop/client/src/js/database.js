cf7dae409dca3d68cfdc7d5da3a70b0be98b739d
samsiebert <sammsiebert@gmail.com>
2022-09-15 19:59:17 -0500

    client src javascript

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  export const getDb = async () => {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    
+    console.log('data saved to database', result);
+  
+    //console.error('getDb not implemented');
+  };  
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (id, value) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jate.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ id: id, value: value });
+  const result = await request;
+  console.log('data saved to database', result);
+
+  
+ // console.error('putDb not implemented');
+};
 
 initdb();
